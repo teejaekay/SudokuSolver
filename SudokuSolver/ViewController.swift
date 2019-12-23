af15ccc8b358f5b40d929212e24a484e821bae1a
Taylor Kelly <taylorkelly27@yahoo.com>
2019-12-23 14:09:28 -0700

    Initial Commit

---
+++ SudokuSolver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SudokuSolver
+//
+//  Created by Taylor Kelly on 12/23/19.
+//  Copyright © 2019 Taylor Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
